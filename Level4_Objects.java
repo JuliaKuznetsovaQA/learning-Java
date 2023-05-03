459d19b2f5fe1b3658e0123172494be2da26e491
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2023-05-03 21:24:28 +0300

    Create Level4_Objects.java

---
+++ Level4_Objects.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String a = new String("Привет");
+        String b = new String("Привет");
+        String c = new String("Привет");
+        String d = new String("Привет");
+        String e = new String("Привет");
+        String f = new String("Привет");
+        String g = new String("Привет");
+        String h = new String("Привет");
+        String i = new String("Привет");
+        String j = new String("Привет");
+        int[] data1 = new int[10];
+        int[] data2 = new int[8];
+        int[] data3 = new int[7];
+        int[] data4 = new int[6];
+        int[] data5 = new int[5];
+        Scanner console = new Scanner("");
+        Scanner sc = new Scanner("");
+    }
+}
