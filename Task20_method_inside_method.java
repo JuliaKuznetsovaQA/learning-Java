58b8a41a4116cbd517d7c8298306f2affce25eb1
Julia Kuznetsova <104379243+JuliaKuznetsovaQA@users.noreply.github.com>
2023-05-06 20:55:22 +0300

    Update Task20_method_inside_method.java

--- Task20_method_inside_method.java
+++ Task20_method_inside_method.java
@@ -6,14 +6,12 @@ package com.javarush.task.pro.task06.task0609;
 
 public class Solution {
     public static void main(String[] args) {
-
     }
-
+    
     public static long cube(long a){
         return a*a*a;
     }
 
-    //напишите тут ваш код
     public static long ninthDegree(long b) {
         return cube(cube(b));
     }
